9593acb0237d846c3b0e2d193df76f8ac651f4d3
hubbub-inc <matthewcarrella9@gmail.com>
2022-01-21 16:54:43 -0500

    sixth commit

--- nami2022/settings.py
+++ nami2022/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,14 +117,18 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-if DEBUG:
-    STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
+# if DEBUG:
+#     STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
